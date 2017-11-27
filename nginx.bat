c27639e0ffc513864d0ab7d9eeb95fa07e3e71db
zhanglianxin <zhanglianxin1993@gmail.com>
2017-11-27 16:00:35 +0800

    Update nginx.bat

--- nginx.bat
+++ nginx.bat
@@ -2,6 +2,6 @@
 title enginx switch by @zhanglianxin
 color 0a
 echo.
-rem 加括号帮助理解，并不影响实际执行效果
+REM Place it in the nginx directory
 (taskkill /f /IM nginx.exe 1>nul 2>nul && echo nginx stopping...) || (start nginx.exe && echo nginx starting...)
 ping 127.0.0.1 >nul 2>nul
